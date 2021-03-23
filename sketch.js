9ea9edde96c70345d2dac3a94efc29376f4e9e18
adin456 <78431494+adin456@users.noreply.github.com>
2021-03-23 12:04:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var car,wall,speed,weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90)
+  weight=random(400,1500)
+
+  car =createSprite(50, 200, 50, 50);
+  wall =createSprite(1500,200,60,height/2);
+
+  car.velocityX = speed;
+
+}
+
+function draw() {
+
+  background(255,255,255);  
+  d = 0.5*weight*speed*speed/22500;
+  if(wall.x-car.x<wall.width/2+car.width/2){
+    car.velocityX=0;
+    if(d<100){
+      car.shapeColor ="green";
+    }
+    if(d>100 && d<180){
+      car.shapeColor="yellow";
+
+    }
+    if (d>180){
+      car.shapeColor="red";
+    }
+  }
+
+  drawSprites();
+}
